0996cc6813cc8501213e1aa8c2adcc77f0ab1259
Liondance <enzo@ieee.org>
2013-07-10 21:50:09 +0000

    Change to read response from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  var string = buffer.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
