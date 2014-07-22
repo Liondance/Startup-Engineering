aede80a375f53701cd129ccd792e132ad17361e7
Enzo Alda <enzo@ieee.org>
2014-07-22 19:33:35 -0400

    .

--- web.js
+++ web.js
@@ -19,8 +19,9 @@ db.sequelize.sync().complete(function(err) {
   if (err) {
     throw err;
   } else {
+  }
+});
+
     http.createServer(app).listen(app.get('port'), function() {
       console.log("Listening on " + app.get('port'));
     });
-  }
-});
